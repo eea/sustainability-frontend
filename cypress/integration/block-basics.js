ac3946b8aae676de1f4f8d3ce7373101229194bc
Nilesh <nileshgulia@gmail.com>
2022-03-17 17:00:19 +0530

    Update block-basics.js

--- cypress/integration/block-basics.js
+++ cypress/integration/block-basics.js
@@ -9,13 +9,12 @@ describe('Blocks Tests', () => {
     // Change page title
     changePageTitle('My Add-on Page');
     // Add block
-    addBlock('Common blocks', 'common_blocks', 'image');
-    cy.get('.block.inner.text').type('My Add-on Page');
+   
 
     // Save
     save('/cypress/my-page');
     // then the page view should contain our changes
     cy.contains('My Add-on Page');
-    cy.get('.block.image');
+   
   });
 });
